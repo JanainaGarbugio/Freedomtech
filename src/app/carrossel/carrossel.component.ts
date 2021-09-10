90a13952cc06980842ce0d98ea681ed3628cdbdb
JanainaGarbugio <contato.jgarbugio@gmail.com>
2021-09-10 09:26:21 -0300

    Fron-end do projeto integrador | 10/09

--- src/app/carrossel/carrossel.component.ts
+++ src/app/carrossel/carrossel.component.ts
@@ -1,3 +1,21 @@
+/*
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel',
+  templateUrl: './carrossel.component.html',
+  styleUrls: ['./carrossel.component.css']
+})
+export class CarrosselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
